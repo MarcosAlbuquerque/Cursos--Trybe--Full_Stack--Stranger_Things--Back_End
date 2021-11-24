ec5c5d485864f72676f2d2a2cad493d9e67f781a
Marcos Albuquerque <mpdsa20@gmail.com>
2021-11-24 17:36:15 -0300

    Revert "Adicionado modulo 'dotenv' para converter arquivo em variaveis de ambiente"
    This reverts commit b543bf3b105424ca63bfc722d785baa52b802dc7.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
 
+const PORT = process.env.PORT || 3000
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
